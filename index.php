756fc0b0c5439a4b541b7057e6b5bc01f6fae73d
Юрий <splate@inbox.ru>
2023-03-12 19:08:09 +0300

    '123'

---
+++ index.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Новости банка");
+?>
+<p>content</p>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/templates/voguish_index/description.php
@@ -0,0 +1,6 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?php
+$arTemplate= [
+    'NAME'=> 'Шаблон главной страницы сайта',
+    'DESCRIPTION'=> 'Описание шаблона',
+];
